45cda309136305ce675e6d103ab84fce6e5e8750
jwest <jwest650gmail.com>
2021-12-01 14:23:26 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return <h1>Codetrain is awesome, react works!!!.</h1>;
+    }
+}
+export default App;
